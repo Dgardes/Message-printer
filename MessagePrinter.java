fc8f3f830517c12f86a2b641cb725606e0007eb4
Dgardes <Artem.Drach.Ua@gmail.com>
2025-12-03 23:58:10 +0200

    Add author information

--- MessagePrinter.java
+++ MessagePrinter.java
@@ -12,14 +12,19 @@ public static String getWelcomeMessage() {
         return "Thank you for using MessagePrinter!";
     }
 
+    public static String getAuthorMessage() {
+        return "Created by: Your Name";
+    }
 
-    
+
     public static void main(String[] args) {
         System.out.println(getWelcomeMessage());
         System.out.println(getDateMessage());
+        System.out.println(getAuthorMessage());
         System.out.println("Learning remote repositories!");
         System.out.println(getGoodbyeMessage());
     }
+
 }
 
 
